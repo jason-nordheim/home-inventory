7e9b5c51398e3de44c8299c846b3680de788bb00
Jason Nordheim <jason.nordheim@gmail.com>
2020-07-29 17:25:39 -0600

    cleared out unused code

--- web-ui/src/Hooks/useAPI.js
+++ web-ui/src/Hooks/useAPI.js
@@ -1,19 +1,6 @@
-import { useState } from 'react'
 
-const createUser = ({ first, last, email, bio, username, password }) => {};
-const getUserToken = ({ username, password }) => {};
+export const useAPI = (token) => {
+  
 
-
-
-export const useAPI = (baseUrl = "http://localhost:4000") => {
-  const [isLoading, setIsLoading] = useState(false) 
-  const [isLoggedIn, setIsLoggedIn] = useState(false) 
-
-  const getItems = () => {}
-  const getLocations = () => {}
-  const getVendors = () => {} 
-
-
-
-  return [isLoading] 
+  return [] 
 }
