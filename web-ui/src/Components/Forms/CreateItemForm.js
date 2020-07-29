41726e2ddab7614b1e1005d5270f09493ba4ebd4
Jason Nordheim <jason.nordheim@gmail.com>
2020-07-29 17:25:49 -0600

    refactored for readability

--- web-ui/src/Components/Forms/CreateItemForm.js
+++ web-ui/src/Components/Forms/CreateItemForm.js
@@ -1,43 +1,60 @@
-import React from 'react'
-import { LabeledTextInput } from '../LabeledTextInput'
+import React from 'react';
+import { LabeledTextInput } from '../LabeledTextInput';
+import { LabeledUsStateInputSelect } from '../LabeledUsStateInputSelect';
 
-export function CreateItemForm(){
+export function CreateItemForm() {
+	function handleNameChanged(event) {}
+	function handleEmailChanged(event) {}
+	function handlePhoneChanged(event) {}
+	function handleStreet1Changed(event) {}
+	function handleStreet2Changed(event) {}
+	function handleCitChanged(event) {}
+	function handleStateChanged(event) {}
 
-    function handleNameChanged(event){}
-    function handleEmailChanged(event){}
-    function handlePhoneChanged(event){} 
-
-    return (
-        <form>
-            <div>
-                <h1>New Item</h1>
-            </div>
-            <div>
-                <LabeledTextInput htmlFor="name" label="Name" placeholder="John Smith or Apple" onValueChanged={handleNameChanged} />
-                <LabeledTextInput htmlFor="email" label="Email" placeholder="email@domain.com" onValueChanged={handleEmailChanged} />
-                <LabeledTextInput htmlFor="phone" label="Phone" placeholder="123-456-7890" onValueChanged={handlePhoneChanged} />
-                <LabeledTextInput htmlFor="street" label="Steet 1" placeholder="2600 Colorado Avenue" onValueChanged={handlePhoneChanged} />
-                <LabeledTextInput htmlFor="street" label="Steet 1" placeholder="Unit 412" onValueChanged={handlePhoneChanged} />
-                <LabeledTextInput htmlFor="city" label="City" placeholder="Boulder" onValueChanged={handlePhoneChanged} />
-            </div>
-           
-            <div>
-                <h4>Address</h4>
-            </div>
-            <div>
-                <label htmlFor="street1">
-                    Street (line 1)
-                </label>
-                <input htmlFor="street1" placeholder="2600 Smith Street"/> 
-            </div>
-            <div>
-                <label htmlFor="street2">
-                    Street (line 2) 
-                </label>
-                <input htmlFor="street2" placeholder="Apartment 403"/> 
-            </div>
-        </form>
-    )
+	return (
+		<form>
+			<div>
+				<h1>New Item</h1>
+			</div>
+			<div>
+				<LabeledTextInput
+					htmlFor="name"
+					label="Name"
+					placeholder="John Smith or Apple"
+					onValueChanged={handleNameChanged}
+				/>
+				<LabeledTextInput
+					htmlFor="email"
+					label="Email"
+					placeholder="email@domain.com"
+					onValueChanged={handleEmailChanged}
+				/>
+				<LabeledTextInput
+					htmlFor="phone"
+					label="Phone"
+					placeholder="123-456-7890"
+					onValueChanged={handlePhoneChanged}
+				/>
+				<LabeledTextInput
+					htmlFor="street1"
+					label="Steet 1"
+					placeholder="2600 Colorado Avenue"
+					onValueChanged={handleStreet1Changed}
+				/>
+				<LabeledTextInput
+					htmlFor="street2"
+					label="Steet 1"
+					placeholder="Unit 412"
+					onValueChanged={handleStreet2Changed}
+				/>
+				<LabeledTextInput
+					htmlFor="city"
+					label="City"
+					placeholder="Boulder"
+					onValueChanged={handlePhoneChanged}
+				/>
+				<LabeledUsStateInputSelect onInputChanged={handleStateChanged} />
+			</div>
+		</form>
+	);
 }
-
-
