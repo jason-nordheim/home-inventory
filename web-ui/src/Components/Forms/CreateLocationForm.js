97a83dde055afd916f06dd62645c307066b084ba
jason-nordheim <jason.nordheim@gmail.com>
2020-07-29 11:51:52 -0600

    created and styled CreateLocationForm

--- web-ui/src/Components/Forms/CreateLocationForm.js
+++ web-ui/src/Components/Forms/CreateLocationForm.js
@@ -1,61 +1,105 @@
 import React from "react";
 import { States } from '../../helpers/state'
+import './CreateLocationForm.css'
 
-export function createLocationForm({ userState, userActions }) {
+export function CreateLocationForm({ userState, userActions, location, locationActions }) {
+  const location_types = {
+    undefined: "Undefined", 
+    house: "House",
+    condo: "Condominium",
+    apartment: "Apartment", 
+    storage: "Storage Unit", 
+    other: "Other"
+  };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault() 
+    console.log(location)
+    alert(location)
+  }
+
   return (
     <form>
       <div>
-        <label htmlFor="name">
-          Location Name 
-        </label>
-        <input /> 
+        <h1>New Location</h1>
       </div>
       <div>
-        <label htmlFor="street1">
-          Street 1 
-        </label>
-        <input /> 
+        <label htmlFor="name">Name</label>
+        <input
+          defaultValue={location.name || ""}
+          onChange={(e) => locationActions.setName(e.target.value)}
+          type="text"
+          placeholder="Name"
+        />
       </div>
       <div>
-        <label htmlFor="street2">
-          Street 2
-        </label>
-        <input /> 
+        <label htmlFor="street1">Street 1</label>
+        <input
+          defaultValue={location.street1 || ""}
+          onChange={(e) => locationActions.setStreet1(e.target.value)}
+          type="text"
+          placeholder="Street 1"
+        />
       </div>
       <div>
-        <label htmlFor="city">
-          City
-        </label>
-        <input /> 
+        <label htmlFor="street2">Street 2</label>
+        <input
+          defaultValue={location.street2 || ""}
+          onChange={(e) => locationActions.setStreet2(e.target.value)}
+          type="text"
+          placeholder="Street 2"
+        />
       </div>
       <div>
-        <label htmlFor="state">
-          State
-        </label>
-        <select>
-          { States.map(state => {
-            return <input value={state.name}>{state.abbreviation}</input>
+        <label htmlFor="city">City</label>
+        <input
+          defaultValue={location.city || ""}
+          onChange={(e) => locationActions.setCity(e.target.value)}
+          type="text"
+          placeholder="City"
+        />
+      </div>
+      <div>
+        <label htmlFor="state">State</label>
+        <select
+          defaultValue={location.state || ""}
+          onChange={(e) => locationActions.setState(e.target.value)}
+        >
+          {States.map((state) => {
+            return (
+              <option key={state.abbreviation} value={state.name}>
+                {state.abbreviation}
+              </option>
+            );
           })}
         </select>
       </div>
       <div>
-        <label htmlFor="zip">
-          Zip Code
-        </label>
-        <input placeholder="Zip Code" /> 
+        <label htmlFor="zip">Zip Code</label>
+        <input
+          value={location.zip || ""}
+          onChange={(e) => locationActions.setZip(e.target.value)}
+          type="text"
+          placeholder="Zip Code"
+        />
       </div>
       <div>
-        <label htmlFor="type">
-          Type
-        </label>
-        <select htmlFor="type">
-          <option value="house">House</option>
-          <option value="condo">Condo</option>
-          <option value="apartment">Apartment</option>
-          <option value="storage unit">Storage Unit</option>
-          <option value="other">Other</option>
+        <label htmlFor="type">Type</label>
+        <select
+          defaultValue={location.type || location_types[0].value }
+          onChange={(e) => locationActions.setType(e.target.value)}
+          htmlFor="type"
+        >
+          {Object.entries(location_types).map(([key, value]) => (
+            <option htmlFor="type" key={key} value={key}>
+              {value}
+            </option>
+          ))}
         </select>
       </div>
+      <div>
+        <button onClick={handleSubmit}>Submit Me</button>
+      </div>
     </form>
-  )
+  );
 }
