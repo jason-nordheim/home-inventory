1ca205dbd955a98d6d6074ca6513ea4f45e45823
Jason Nordheim <jason.nordheim@gmail.com>
2020-07-31 11:28:19 -0600

    updated header with active links and link to account

--- web-ui/src/Components/Header.js
+++ web-ui/src/Components/Header.js
@@ -1,20 +1,33 @@
 import React from 'react' 
+import pages from './Pages'
+import { NavLink, Link } from 'react-router-dom'
 import './Header.css'
+
+
 
 export default function Header(){
     return (
         <header>
-            <aside>
+            <aside style={{ fontVariant: 'small-caps'}}>
                 Home Inventory 
             </aside>
             <nav>
                 <ul>
-                    <li>Home</li>
-                    <li>Items</li>
-                    <li>Locations</li>
-                    <li>Vendors</li>
+                    { pages.map( page =>  <CustomLink {...page} /> ) }
                 </ul>
             </nav>
+            <aside>
+                <Link to="/account">
+                    <i class="fas fa-user-circle"></i>
+                </Link>
+            </aside>
         </header>
     )
+}
+
+function CustomLink({ exact, key, path, display}) {
+    return <li key={key}>
+        <NavLink exact={exact} to={path} activeStyle={{ fontWeight: 'bold', fontSize: '1.1rem'}}>{display}</NavLink>
+    </li>
+
 }
