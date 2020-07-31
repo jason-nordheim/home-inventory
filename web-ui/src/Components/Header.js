d99708a26bbf4b02ae371e9a1ed5f83dae35928c
Jason Nordheim <jason.nordheim@gmail.com>
2020-07-31 14:38:57 -0600

    created custom link component to render nav links

--- web-ui/src/Components/Header.js
+++ web-ui/src/Components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react' 
 import pages from './Pages'
+import { AccountDetails } from './AccountDetails'
 import { NavLink, Link } from 'react-router-dom'
 import './Header.css'
 
 
 
-export default function Header(){
+export default function Header({userState, userActions}){
+    
     return (
         <header>
             <aside style={{ fontVariant: 'small-caps'}}>
@@ -13,21 +15,25 @@ export default function Header(){
             </aside>
             <nav>
                 <ul>
-                    { pages.map( page =>  <CustomLink {...page} /> ) }
+                    { pages.map( page =>  <CustomLink key={page.key} {...page} /> ) }
                 </ul>
             </nav>
             <aside>
                 <Link to="/account">
-                    <i class="fas fa-user-circle"></i>
+                    <i className="fas fa-user-circle"></i>
                 </Link>
             </aside>
         </header>
     )
 }
 
-function CustomLink({ exact, key, path, display}) {
-    return <li key={key}>
-        <NavLink exact={exact} to={path} activeStyle={{ fontWeight: 'bold', fontSize: '1.1rem'}}>{display}</NavLink>
-    </li>
+function CustomLink({ exact, path, display, nav}) {
+    if (nav) {
+        return <li>
+            <NavLink exact={exact} to={path} activeStyle={{ fontWeight: 'bold', fontSize: '1.1rem'}}>{display}</NavLink>
+        </li>
+    } else {
+        return null 
+    }
 
 }
