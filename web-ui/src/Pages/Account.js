6fddceddf283a59433ec88103d6afbbc763e068d
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-06 15:50:50 -0600

    wrapped AccountPage in "withAuthentication"

--- web-ui/src/Pages/Account.js
+++ web-ui/src/Pages/Account.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Layout from '../Components/Layout'
 import SignInForm from '../Components/SignInForm'
 import RegistrationForm from '../Components/RegistrationForm'
+import withAuthentication from '../Components/withAuthentication'
 
-const AccountPage = () => (
+const AccountPage = props => (
     <Layout title="Account">
         <SignInForm /> 
         <RegistrationForm /> 
     </Layout>
 )
 
-export default AccountPage 
+export default withAuthentication(AccountPage) 
