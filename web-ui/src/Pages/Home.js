d83ac179a42231401e6ecd71eb2e54663d98d356
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-06 15:51:03 -0600

    wrapped HomePage in withAuthetnication

--- web-ui/src/Pages/Home.js
+++ web-ui/src/Pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react' 
 import Layout from '../Components/Layout'
+import withAuthentication from '../Components/withAuthentication'
 
-const HomePage = () => (
+const HomePage = props => (
     <Layout title="Home">
         
     </Layout>
 )
 
-export default HomePage 
+export default withAuthentication(HomePage) 
