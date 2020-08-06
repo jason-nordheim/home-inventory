95ad0af93374bba5c3c5073e12fbcd62b9135501
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-06 15:50:17 -0600

    added footer to layout

--- web-ui/src/Components/Layout.js
+++ web-ui/src/Components/Layout.js
@@ -1,12 +1,16 @@
 import React from 'react' 
 import PersistentDrawerLeft from './PersistentDrawerLeft'
+import StickyFooter from './StickyFooter'
 
 const Layout = ({children, title}) => {
     return (
-        <PersistentDrawerLeft> 
-            { title &&  <h1 style={{display: 'flex', placeContent: 'center'}}>{title}</h1> }
-            { children }
-        </PersistentDrawerLeft> 
+        <>
+            <PersistentDrawerLeft> 
+                { title &&  <h1 style={{display: 'flex', placeContent: 'center'}}>{title}</h1> }
+                { children }
+            </PersistentDrawerLeft> 
+            <StickyFooter /> 
+        </> 
     )
 }
 
