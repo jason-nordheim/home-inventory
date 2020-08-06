553582ae8bef7c8c3a949cc1dc906572818e0848
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-06 11:38:57 -0600

    handle input events

--- web-ui/src/Components/RegistrationForm.js
+++ web-ui/src/Components/RegistrationForm.js
@@ -2,26 +2,67 @@ import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import useStyles from './SignInForm.styles';
 
-const SignInForm = () => {
+const RegistrationForm = () => {
 	const [ username, setUsername ] = useState('');
 	const [ usernameError, setUsernameError ] = useState(null);
 	const [ password, setPassword ] = useState('');
 	const [ passwordError, setPasswordError ] = useState(null);
 	const [ name, setName ] = useState('');
 	const [ nameError, setNameError ] = useState(null);
-	const classes = useStyles();
+	const [ email, setEmail ] = useState('');
+    const [ emailError, setEmailError ] = useState(null);
+    const [ phone, setPhone] = useState('')
+    const [ phoneError, setPhoneError] = useState(null)
+    const classes = useStyles();
+    const MIN_CHARS = 3 
 
 	const handleUsernameInput = (e) => {
-		setUsername(e.target.value);
+        setUsername(e.target.value);
+        if (e.target.value.length < 3 && e.target.value.length != 0) {
+            setUsernameError('Username too short')
+        } else {
+            setUsernameError(null)
+        }
 	};
 
 	const handlePasswordInput = (e) => {
-		setPassword(e.target.value);
+        setPassword(e.target.value);
+        if (e.target.value.length < MIN_CHARS && e.target.value.length != 0) {
+            setPasswordError('Password too short')
+        } else {
+            setPasswordError(null)
+        }
 	};
 
 	const handleNameInput = (e) => {
-		setName(e.target.value);
+        setName(e.target.value);
+        if (e.target.value.length < MIN_CHARS && e.target.value.length != 0) {
+            setNameError('Name too short')
+        } else {
+            setNameError(null)
+        }
 	};
+
+	const handleEmailInput = (e) => {
+        setEmail(e.target.value);
+        if (e.target.value.length < MIN_CHARS && e.target.value.length != 0) {
+            setEmailError('Invalid Email Address')
+        } else {
+
+        }
+    };
+    
+    const handlePhoneInput = (e) => {
+        setPhone(e.target.value)
+        if (e.target.value.length < 10) {
+            setPhoneError('Invalid Phone Number')
+        }
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault() 
+    }
+
 	return (
 		<Paper>
 			<form className={classes.root}>
@@ -30,31 +71,57 @@ const SignInForm = () => {
 				</div>
 				<div>
 					<TextField
-						error={nameError}
-						id="username"
-						label="Username"
-						defaultValue={name}
-						onChange={handleNameInput}
+						error={nameError !== null}
+						id="name"
+						label="Name"
+                        defaultValue={name}
+                        helperText={ nameError == null ? '' : nameError }
+                        onChange={handleNameInput}
+                        required
+					/>
+					<TextField
+						error={emailError !== null}
+						id="email"
+						label="Email"
+                        defaultValue={email}
+                        helperText={ emailError == null ? '' : emailError }
+                        onChange={handleEmailInput}
+                        required
+					/>
+					<TextField
+						error={phoneError !== null}
+						id="phone"
+						label="Phone"
+                        defaultValue={phone}
+                        helperText={ phoneError == null ? '' : phoneError }
+                        onChange={handlePhoneInput}
+                        required
 					/>
 				</div>
 				<div>
 					<TextField
-						error={usernameError}
+						error={usernameError !== null }
 						id="username"
 						label="Username"
-						defaultValue={username}
-						onChange={handleUsernameInput}
+                        defaultValue={username}
+                        helperText={ usernameError == null ? '' : usernameError }
+                        onChange={handleUsernameInput}
+                        required
 					/>
 					<TextField
-						error={passwordError}
+						error={passwordError != null}
 						id="password"
-						label="Password"
-						defaultValue={passwordError}
-						onChange={handlePasswordInput}
+                        label="Password"
+                        defaultValue={password}
+                        helperText={ passwordError == null ? '' : passwordError }
+                        onChange={handlePasswordInput}
+                        required
 					/>
+				</div>
+				<div>
 					<span style={{ alignSelf: 'center' }}>
-						<Button variant="contained" color="primary">
-							Primary
+						<Button variant="contained" color="primary" onClick={handleSubmit}>
+							Register
 						</Button>
 					</span>
 				</div>
@@ -64,4 +131,4 @@ const SignInForm = () => {
 	);
 };
 
-export default SignInForm;
+export default RegistrationForm;
