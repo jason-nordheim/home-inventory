38bf42e9044e42d14a7bd1a95bfc7e3b57933206
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-06 16:07:14 -0600

    wrapped pages in HOC "withAuthentication"

--- web-ui/src/Pages/Account.js
+++ web-ui/src/Pages/Account.js
@@ -1,14 +1,17 @@
-import React from 'react' 
-import Layout from '../Components/Layout'
-import SignInForm from '../Components/SignInForm'
-import RegistrationForm from '../Components/RegistrationForm'
-import withAuthentication from '../Components/withAuthentication'
+import React from 'react';
+import Layout from '../Components/Layout';
+import SignInForm from '../Components/SignInForm';
+import RegistrationForm from '../Components/RegistrationForm';
+import withAuthentication from '../Components/withAuthentication';
 
-const AccountPage = props => (
-    <Layout title="Account">
-        <SignInForm /> 
-        <RegistrationForm /> 
-    </Layout>
-)
+const AccountPage = (props) => {
+	const { Authenticator } = props;
+	return (
+		<Layout title="Account">
+			<SignInForm Authenticator={Authenticator}/>
+			<RegistrationForm Authenticator={Authenticator}/>
+		</Layout>
+	);
+};
 
-export default withAuthentication(AccountPage) 
+export default withAuthentication(AccountPage);
--- web-ui/src/Pages/Home.js
+++ web-ui/src/Pages/Home.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Layout from '../Components/Layout'
 import withAuthentication from '../Components/withAuthentication'
 
-const HomePage = props => (
-    <Layout title="Home">
-        
-    </Layout>
-)
+const HomePage = props => {
+    const { Authenticator } = props 
+    return (
+        <Layout title="Home">
+        </Layout>
+    )
+}
 
 export default withAuthentication(HomePage) 
