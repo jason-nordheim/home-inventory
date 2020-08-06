09948e8aa62b1e45ba33cf11370998835ceab3a8
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-06 16:06:58 -0600

    refactoring of code into distinct files

--- web-ui/src/Components/StickyFooter.js
+++ web-ui/src/Components/StickyFooter.js
@@ -1,40 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 
-import { Typography, Container, Link } from '@material-ui/core'
+import { Typography, Container } from '@material-ui/core'
+import { useStyles } from './StickFooter.styles';
+import { Copyright } from './Copyright';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Jason Nordheim
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    position: 'fixed', 
-    bottom: 0, 
-    width: '100%', 
-    zIndex: -1, 
-  },
-  footer: {
-    padding: theme.spacing(2, 1),
-    textAlign: 'center', 
-    marginTop: 'auto',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
-  },
-}));
-
-export default function StickyFooter() {
+const StickyFooter = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
@@ -47,3 +17,5 @@ export default function StickyFooter() {
     </div>
   );
 }
+
+export default StickyFooter
