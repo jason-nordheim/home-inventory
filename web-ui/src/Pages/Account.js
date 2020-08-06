b69425f9073b27b1cfc40de1499044f8114c6b68
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-06 11:39:21 -0600

    added the registration form to the account page

--- web-ui/src/Pages/Account.js
+++ web-ui/src/Pages/Account.js
@@ -1,10 +1,12 @@
 import React from 'react' 
 import Layout from '../Components/Layout'
 import SignInForm from '../Components/SignInForm'
+import RegistrationForm from '../Components/RegistrationForm'
 
 const AccountPage = () => (
     <Layout title="Account">
         <SignInForm /> 
+        <RegistrationForm /> 
     </Layout>
 )
 
