7a1fd8005c403e37689ccb572458fca7c11e5834
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-06 09:51:38 -0600

    created account page

---
+++ web-ui/src/Pages/Account.js
@@ -0,0 +1,10 @@
+import React from 'react' 
+import Layout from '../Components/Layout'
+
+const AccountPage = () => (
+    <Layout>
+        <h1>Account</h1>
+    </Layout>
+)
+
+export default AccountPage 
