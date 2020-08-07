1ecd5b305c13c7c24490f2a5fc5d71265957de2a
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-07 16:28:51 -0600

    removed try catch from authenticator

--- web-ui/src/util/Authenticator.js
+++ web-ui/src/util/Authenticator.js
@@ -1,40 +1,30 @@
 const axios = require('axios').default;
 
 class Authenticator {
-    constructor(baseUrl="http://localhost:3000"){
-        this.baseUrl = baseUrl 
-        this.isLoggedIn = false; 
-        this.user = null; 
-    }
+	constructor(baseUrl = 'http://localhost:3000') {
+		this.baseUrl = baseUrl;
+		this.isLoggedIn = false;
+		this.user = null;
+	}
 
-    async register({ username, password, email, phone }) {
-        try {
-            const res =  await axios.post(this.baseUrl + "/users", {username, password , email, phone })
-            console.log(res)
-            return res
-        } catch (error) {
-            console.error("Unable to Register")
-            return error
-        }
-    }
+	async register({ username, password, email, phone }) {
+        const res = await axios.post(this.baseUrl + '/users', { username, password, email, phone });
+        console.log('axios', res);
+        return res;
+	}
 
-    async login({username, password}){
-        try {
-            const res = await axios.post(this.baseUrl + "/login", { username, password })
-            console.log(res)
-            return res 
-        } catch (error) {
-            console.error("Unable to login")
-            return error
-        }
-    }
+	async login({ username, password }) {
+		const res = await axios.post(this.baseUrl + '/login', { username, password });
+		console.log('axios', res);
+		return res;
+	}
 
-    logout() {
-        this.isLoggedIn = false; 
-        this.user = null; 
-    }
+	logout() {
+		this.isLoggedIn = false;
+		this.user = null;
+	}
 }
 
 /** Singleton Pattern  */
-const MyAuthenticator = new Authenticator() 
-export default MyAuthenticator
+const MyAuthenticator = new Authenticator();
+export default MyAuthenticator;
