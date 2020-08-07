b565fc27de09ce5aac7499d16e44785f3ab7a106
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-07 14:02:36 -0600

    transistioned sign-in form to use grid

--- web-ui/src/Components/SignInForm.js
+++ web-ui/src/Components/SignInForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import { TextField, Button, Typography, Paper, Grid } from '@material-ui/core';
 import useStyles from './SignInForm.styles';
 
 const SignInForm = ({ Authenticator }) => {
@@ -28,29 +28,39 @@ const SignInForm = ({ Authenticator }) => {
 		e.preventDefault();
 	};
 	return (
-		<Paper>
-			<form className={classes.root}>
-				<div>
-					<Typography variant="h5">Sign In</Typography>
-				</div>
-				<div>
-					<TextField
-						error={usernameError}
-						id="username"
-						label="Username"
-						defaultValue={username}
-						onChange={handleUsernameInput}
-						required
-					/>
-					<TextField
-						error={passwordError}
-						id="password"
-						label="Password"
-						defaultValue={passwordError}
-						onChange={handlePasswordInput}
-						required
-					/>
-					<span style={{ alignSelf: 'center' }}>
+		<Paper style={{ margin: '1rem 1rem', padding: '2rem 1rem' }}>
+			<form>
+				<Grid container spacing={2}>
+					<Grid item xs={12} style={{ textAlign: 'center' }}>
+						<Typography variant="h5">Sign In</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Grid container spacing={2} justify="center">
+							<Grid item xs={10} sm={5} md={4} lg={3}>
+								<TextField
+									fullWidth
+									error={usernameError}
+									id="username"
+									label="Username"
+									defaultValue={username}
+									onChange={handleUsernameInput}
+									required
+								/>
+							</Grid>
+							<Grid item xs={10} sm={5} md={4} lg={3}>
+								<TextField
+									fullWidth
+									error={passwordError}
+									id="password"
+									label="Password"
+									defaultValue={passwordError}
+									onChange={handlePasswordInput}
+									required
+								/>
+							</Grid>
+						</Grid>
+					</Grid>
+					<Grid item xs={12}  style={{textAlign: 'center'}}>
 						<Button
 							variant="contained"
 							color="primary"
@@ -64,9 +74,8 @@ const SignInForm = ({ Authenticator }) => {
 						>
 							Sign In
 						</Button>
-					</span>
-				</div>
-				<div />
+					</Grid>
+				</Grid>
 			</form>
 		</Paper>
 	);
