e9660e17b9256ac40ce1b18b885ec3de2f6aab8c
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-07 16:28:10 -0600

    added a timeout for error messages

--- web-ui/src/Components/SignInForm.js
+++ web-ui/src/Components/SignInForm.js
@@ -1,45 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import { TextField, Button, Typography, Paper, Grid } from '@material-ui/core';
+import showErrorMessage from './ShowErrorMessage';
 
 const SignInForm = ({ Authenticator }) => {
 	const [ username, setUsername ] = useState('');
 	const [ usernameError, setUsernameError ] = useState(null);
 	const [ password, setPassword ] = useState('');
 	const [ passwordError, setPasswordError ] = useState(null);
+	const [ errorMessage, setErrorMessage] = useState(null)
 	const MIN_CHARS = 3;
-	const hasWhiteSpaceRegEx = new RegExp(/\s/)
+	const hasWhiteSpaceRegEx = new RegExp(/\s/);
+
+	// remove the error message after 2 seconds 
+	useEffect(() => {
+		if (errorMessage !== null) {
+			setTimeout(() => {
+				setErrorMessage(null)
+			}, 2000)
+		}
+	}, [errorMessage])
 
 	const handleUsernameInput = (e) => {
 		setUsername(e.target.value);
 		if (e.target.value.length < MIN_CHARS && e.target.value.length !== 0) {
 			setUsernameError('Username too short');
 		} else if (hasWhiteSpaceRegEx.test(e.target.value)) {
-			setUsernameError('Username cannot have spaces')
+			setUsernameError('Username cannot have spaces');
 		} else {
-			setUsernameError(null)
+			setUsernameError(null);
 		}
 	};
-
+	
 	const handlePasswordInput = (e) => {
 		setPassword(e.target.value);
 		if (e.target.value.length < MIN_CHARS && e.target.value.length !== 0) {
 			setPasswordError('Password too short');
 		} else if (hasWhiteSpaceRegEx.test(e.target.value)) {
-			setPasswordError('Password cannot have spaces')
+			setPasswordError('Password cannot have spaces');
 		} else {
-			setPasswordError(null)
+			setPasswordError(null);
 		}
 	};
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		const result = Authenticator.login({ username, password })
-		console.log(result)
+		Authenticator.login({ username, password })
+			.then(res => console.log('result', res))
+			.catch(error =>  setErrorMessage(error.message))
 	};
+
 	return (
-		<Paper style={{ margin: '1rem 1rem', padding: '2rem 1rem' }}>
-			<form>
-				<Grid container spacing={2}>
+		<Grid container spacing={2} justify="center" >
+			<Paper style={{ margin: '1rem 1rem', padding: '2rem 1rem' , flex: 1}}>
+				<form>
 					<Grid item xs={12} style={{ textAlign: 'center' }}>
 						<Typography variant="h5">Sign In</Typography>
 					</Grid>
@@ -48,7 +61,7 @@ const SignInForm = ({ Authenticator }) => {
 							<Grid item xs={10} sm={5} md={4} lg={3}>
 								<TextField
 									fullWidth
-									error={usernameError !== null }
+									error={usernameError !== null}
 									id="username"
 									label="Username"
 									helperText={usernameError !== null ? usernameError : ''}
@@ -70,7 +83,7 @@ const SignInForm = ({ Authenticator }) => {
 							</Grid>
 						</Grid>
 					</Grid>
-					<Grid item xs={12}  style={{textAlign: 'center'}}>
+					<Grid item xs={12} style={{ textAlign: 'center' , marginTop: '1rem'}}>
 						<Button
 							variant="contained"
 							color="primary"
@@ -85,9 +98,12 @@ const SignInForm = ({ Authenticator }) => {
 							Sign In
 						</Button>
 					</Grid>
-				</Grid>
-			</form>
-		</Paper>
+					{
+						errorMessage !== null && showErrorMessage(errorMessage)  
+					}
+				</form>
+			</Paper>
+		</Grid>
 	);
 };
 
