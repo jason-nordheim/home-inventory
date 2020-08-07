aeb1d3b758f36b478da203aabf983dff42ff3ca8
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-07 14:49:06 -0600

    de-pluralized table name

--- API/db/migrate/20200805173553_create_users.rb
+++ API/db/migrate/20200805173553_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
+    create_table :user do |t|
       t.string :username,  unique: true
       t.string :password_digest
       t.string :name
