cadeaf9fbf07016c8dbb283fbd4ef2031cdad803
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-10 10:23:08 -0600

    converted styling to use useFormStyles hook and added timeout effect for the error message

--- web-ui/src/Components/SignInForm.js
+++ web-ui/src/Components/SignInForm.js
@@ -1,24 +1,30 @@
-import React, { useState , useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper, Grid } from '@material-ui/core';
+import { useFormStyles } from './Form.Styles'
 import showErrorMessage from './ShowErrorMessage';
+import { Link } from 'react-router-dom';
 
 const SignInForm = ({ Authenticator }) => {
 	const [ username, setUsername ] = useState('');
 	const [ usernameError, setUsernameError ] = useState(null);
 	const [ password, setPassword ] = useState('');
 	const [ passwordError, setPasswordError ] = useState(null);
-	const [ errorMessage, setErrorMessage] = useState(null)
+	const [ errorMessage, setErrorMessage ] = useState(null);
 	const MIN_CHARS = 3;
 	const hasWhiteSpaceRegEx = new RegExp(/\s/);
+	const SPACING = 2;
 
-	// remove the error message after 2 seconds 
-	useEffect(() => {
-		if (errorMessage !== null) {
-			setTimeout(() => {
-				setErrorMessage(null)
-			}, 2000)
-		}
-	}, [errorMessage])
+	// remove the error message after 2 seconds
+	useEffect(
+		() => {
+			if (errorMessage !== null) {
+				setTimeout(() => {
+					setErrorMessage(null);
+				}, 2000);
+			}
+		},
+		[ errorMessage ]
+	);
 
 	const handleUsernameInput = (e) => {
 		setUsername(e.target.value);
@@ -30,7 +36,7 @@ const SignInForm = ({ Authenticator }) => {
 			setUsernameError(null);
 		}
 	};
-	
+
 	const handlePasswordInput = (e) => {
 		setPassword(e.target.value);
 		if (e.target.value.length < MIN_CHARS && e.target.value.length !== 0) {
@@ -45,45 +51,49 @@ const SignInForm = ({ Authenticator }) => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		Authenticator.login({ username, password })
-			.then(res => console.log('result', res))
-			.catch(error =>  setErrorMessage(error.message))
+			.then((res) => console.log('result', res))
+			.catch((error) => setErrorMessage(error.message));
 	};
 
+	const classes = useFormStyles()
+
 	return (
-		<Grid container spacing={2} justify="center" >
-			<Paper style={{ margin: '1rem 1rem', padding: '2rem 1rem' , flex: 1}}>
+		<Grid
+			container
+			justify="center"
+			className={classes.root}
+		>
+			<Paper className={classes.paper}>
 				<form>
-					<Grid item xs={12} style={{ textAlign: 'center' }}>
+					<Grid item>
 						<Typography variant="h5">Sign In</Typography>
 					</Grid>
-					<Grid item xs={12}>
-						<Grid container spacing={2} justify="center">
-							<Grid item xs={10} sm={5} md={4} lg={3}>
-								<TextField
-									fullWidth
-									error={usernameError !== null}
-									id="username"
-									label="Username"
-									helperText={usernameError !== null ? usernameError : ''}
-									onChange={handleUsernameInput}
-									required
-								/>
-							</Grid>
-							<Grid item xs={10} sm={5} md={4} lg={3}>
-								<TextField
-									fullWidth
-									error={passwordError !== null}
-									id="password"
-									label="Password"
-									type="password"
-									helperText={passwordError !== null ? passwordError : ''}
-									onChange={handlePasswordInput}
-									required
-								/>
-							</Grid>
-						</Grid>
+					<Grid item>
+						<TextField
+							fullWidth
+							className={classes.textField}
+							error={usernameError !== null}
+							id="username"
+							label="Username"
+							helperText={usernameError !== null ? usernameError : ''}
+							onChange={handleUsernameInput}
+							required
+						/>
 					</Grid>
-					<Grid item xs={12} style={{ textAlign: 'center' , marginTop: '1rem'}}>
+					<Grid item>
+						<TextField
+							fullWidth
+							className={classes.textField}
+							error={passwordError !== null}
+							id="password"
+							label="Password"
+							type="password"
+							helperText={passwordError !== null ? passwordError : ''}
+							onChange={handlePasswordInput}
+							required
+						/>
+					</Grid>
+					<Grid item>
 						<Button
 							variant="contained"
 							color="primary"
@@ -94,13 +104,14 @@ const SignInForm = ({ Authenticator }) => {
 								password.length < MIN_CHARS ||
 								username.length < MIN_CHARS
 							}
+							style={{
+								marginTop : '1rem'
+							}}
 						>
 							Sign In
 						</Button>
 					</Grid>
-					{
-						errorMessage !== null && showErrorMessage(errorMessage)  
-					}
+					{errorMessage !== null && showErrorMessage(errorMessage)}
 				</form>
 			</Paper>
 		</Grid>
