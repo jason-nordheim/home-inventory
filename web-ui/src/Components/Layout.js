af08c2d2e2b635e4b5c631d600b7f059f826bbad
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-10 17:39:31 -0600

    created title component to render as part of the page content

--- web-ui/src/Components/Layout.js
+++ web-ui/src/Components/Layout.js
@@ -1,27 +1,29 @@
-import React from 'react' 
-import PersistentDrawerLeft from './PersistentDrawerLeft'
+import React from 'react';
+import PersistentDrawerLeft from './PersistentDrawerLeft';
 import { ThemeProvider, Typography, SvgIcon } from '@material-ui/core';
-import theme from '../style/useTheme'
+import theme from '../style/useTheme';
 // import StickyFooter from './StickyFooter'
 
-const Layout = ({children, title}) => {
-    return (
-        <ThemeProvider theme={theme}>
-            <PersistentDrawerLeft> 
-    { title &&  <Title title={title} /> } 
-                { children }
-            </PersistentDrawerLeft> 
-            {/* <StickyFooter />  */}
-        </ThemeProvider>
-    )
-}
+const Layout = ({ children, title }) => {
+	return (
+		<ThemeProvider theme={theme}>
+			<PersistentDrawerLeft>
+				{title && <Title title={title} />}
+				{children}
+			</PersistentDrawerLeft>
+			{/* <StickyFooter />  */}
+		</ThemeProvider>
+	);
+};
 
-const Title = ({title, props}) => {
-    return (
-        <span>
-            <Typography variant="h3" style={{display: 'flex', placeContent: 'center', marginBottom: '2rem'}}>{title}</Typography> 
-        </span> 
-    )
-}
+const Title = ({ title, props }) => {
+	return (
+		<span>
+			<Typography variant="h3" style={{ display: 'flex', placeContent: 'center', marginBottom: '2rem' }}>
+				{title}
+			</Typography>
+		</span>
+	);
+};
 
-export default Layout
+export default Layout;
