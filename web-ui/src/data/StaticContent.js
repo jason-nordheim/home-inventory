d20112fdbdae3ea8a476c0f6703b8b9167226576
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-10 09:12:11 -0600

    created static content file to hold static page text content

---
+++ web-ui/src/data/StaticContent.js
@@ -0,0 +1,24 @@
+export const ApplicationName = 'Sorted' 
+
+export const StaticContent = {
+    PageContent: {
+        About: [
+            {
+                title: `Welcome to ${ApplicationName}`,
+                variant: 'h4', 
+                body: [
+                    `A simple and powerful way to acurately document everything you have at home, in storage, or across multiple properties`
+                ], 
+                images: []
+            }, 
+            {
+                title: `Be Prepared for the bumps in the road`, 
+                variant: 'h5', 
+                body: [
+                    `Life is messy. And you can’t control everything. But you can be prepared. ${ApplicationName} lets you quickly make an accurate and up to date record of all your valuables. Create entries for each item in your house, add photos and detailed product information, like serial number, purchase date, value and warranty.`
+                ],
+                images: []
+            },
+        ],
+    }
+}
