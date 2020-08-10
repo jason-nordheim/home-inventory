fced6e8a5ba365e3e822032e27660ad9bb315f5b
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-10 16:28:33 -0600

    added more static context data for pages

--- web-ui/src/data/StaticContent.js
+++ web-ui/src/data/StaticContent.js
@@ -5,20 +5,22 @@ export const StaticContent = {
         About: [
             {
                 title: `Welcome to ${ApplicationName}`,
-                variant: 'h4', 
                 body: [
                     `A simple and powerful way to acurately document everything you have at home, in storage, or across multiple properties`
                 ], 
-                images: []
             }, 
             {
-                title: `Be Prepared for the bumps in the road`, 
-                variant: 'h5', 
+                title: `Be Prepared for the bumps in the Road`, 
                 body: [
                     `Life is messy. And you can’t control everything. But you can be prepared. ${ApplicationName} lets you quickly make an accurate and up to date record of all your valuables. Create entries for each item in your house, add photos and detailed product information, like serial number, purchase date, value and warranty.`
                 ],
-                images: []
             },
+            {
+                title: 'Start now. Avoid the headache later', 
+                body: [
+                    `Even if the words home inventory don’t make you cringe, you probably have better things to do than document all your stuff. Sortly was created by busy people, for busy people. It’s simple and easy to use–no technical wizardry required. But with all the extra features packed inside you may feel like you’ve got magic in your fingertips.`
+                ]
+            }
         ],
     }
 }
