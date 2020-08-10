07ff56629c24c532cf4576f7ca6e4457e630434e
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-10 17:39:08 -0600

    added AuthorizationContext Provider

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -2,21 +2,24 @@ import React, { createContext } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SitePages from './data/SitePages';
 import useAuthentication from './hooks/useAuthentication'
+import AuthorizationContext from './AuthContext'
 
 const App = () => {
 	const [ state, dispatch ] = useAuthentication()
 	return (
-		<BrowserRouter>
-			<Switch>
-					{SitePages.map((link) => {
-						return (
-							<Route key={link.url} path={link.url} exact={link.exact}>
-								{link.comp}
-							</Route>
-						);
-					})}
-			</Switch>
-		</BrowserRouter>
+		<AuthorizationContext.Provider value={{state, dispatch}} >
+			<BrowserRouter>
+				<Switch>
+						{SitePages.map((link) => {
+							return (
+								<Route key={link.url} path={link.url} exact={link.exact}>
+									{link.comp}
+								</Route>
+							);
+						})}
+				</Switch>
+			</BrowserRouter>
+		</AuthorizationContext.Provider>
 	);
 };
 
