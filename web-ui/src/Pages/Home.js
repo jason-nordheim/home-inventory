61137d8664ca1b83d9a19750ac2613dc7625a6b3
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-10 09:12:29 -0600

    wrapped about page content in "Container" element

--- web-ui/src/Pages/Home.js
+++ web-ui/src/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react' 
 import Layout from '../Components/Layout'
 import withAuthentication from '../Components/withAuthentication'
-import { Typography } from '@material-ui/core'
+import { Typography, Container } from '@material-ui/core'
 
 const HomePage = ({Authenticator}) => {
     return (
@@ -14,7 +14,14 @@ const HomePage = ({Authenticator}) => {
 
 const About = () => {
     return (
-        <>
+        <Container>
+        <Typography variant="h5">
+            Life Is Messy 
+        </Typography>
+        <Typography paragraph>
+        Life is messy. And you can’t control everything. But you can be prepared. Sortly lets you quickly make an accurate and up to date record of all your valuables. Create entries for each item in your house, add photos and detailed product information, like serial number, purchase date, value and warranty.
+        </Typography>
+        <br /> 
         <Typography variant="h5">
             Features
         </Typography>
@@ -39,11 +46,27 @@ const About = () => {
                     <li>Identify items that are should be or are worth selling</li>
                 </ul>
             </li>
+            <li>
+                Be ready for the worst
+                <ul>
+                    <li>
+                    Capture receipts, manuals, contracts and important documents
+                    </li>
+                    <li>
+                    Set reminders for lent items, expiring warranties and return dates
+                    </li>
+                </ul>
+            </li>
+            <ul>
+                <li>
+
+                </li>
+            </ul>
         </ul>
         <Typography paragraph>
         MyHome can replace dozens of applications helping you organize your life and increasing your efficiency
         </Typography>
-        </>
+        </Container>
     )
 }
 
