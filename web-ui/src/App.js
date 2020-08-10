84dca84609efcce2a3d30ad613d6be9904e9c506
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-10 11:24:19 -0600

    moved theme application to layout component

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SitePages from './data/SitePages';
 
-const App =() =>  {
-  return (
-    <BrowserRouter>
-      <Switch>
-        { SitePages.map(link => {
-          return (
-            <Route key={link.url} path={link.url} exact={link.exact}>
-              {link.comp}
-            </Route>
-          )
-        })}
-      </Switch>
-    </BrowserRouter>
-  )
-}
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Switch>
+					{SitePages.map((link) => {
+						return (
+							<Route key={link.url} path={link.url} exact={link.exact}>
+								{link.comp}
+							</Route>
+						);
+					})}
+			</Switch>
+		</BrowserRouter>
+	);
+};
 
 export default App;
--- web-ui/src/Components/Layout.js
+++ web-ui/src/Components/Layout.js
@@ -1,16 +1,18 @@
 import React from 'react' 
 import PersistentDrawerLeft from './PersistentDrawerLeft'
+import { ThemeProvider } from '@material-ui/core';
+import theme from '../style/useTheme'
 // import StickyFooter from './StickyFooter'
 
 const Layout = ({children, title}) => {
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <PersistentDrawerLeft> 
                 { title &&  <h1 style={{display: 'flex', placeContent: 'center'}}>{title}</h1> }
                 { children }
             </PersistentDrawerLeft> 
             {/* <StickyFooter />  */}
-        </> 
+        </ThemeProvider>
     )
 }
 
