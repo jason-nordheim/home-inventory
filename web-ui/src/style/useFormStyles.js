e592d20b2396d2f633caa72d2c0abf3509236c65
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-10 16:30:03 -0600

    created stlying hook for the forms

---
+++ web-ui/src/style/useFormStyles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core'
+
+export const useFormStyles = makeStyles((theme) => ({
+    root: {
+        minWidth: '325px', 
+	    maxWidth: '50vw',
+		textAlign: 'center' 
+    },
+    paper: {
+        margin: 'auto 1rem auto 1rem', 
+        padding: '2rem 3rem', 
+        flex: 1, 
+    },
+    textField: {
+        maxWidth: '30rem', 
+        margin: theme.spacing(1)
+    }
+}))
+
+export default useFormStyles 
