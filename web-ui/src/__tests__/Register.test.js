550a234dfd3959cdc448732ae7c1ac54d0fd7044
jason-nordheim <jason.nordheim@gmail.com>
2020-08-11 09:56:47 -0600

    created register function and consolidated all login functions to a single file

---
+++ web-ui/src/__tests__/Register.test.js
@@ -0,0 +1,5 @@
+import { register } from "../util/Authentication";
+
+describe('Register is a function', () => {
+  expect(typeof(register)).toBe('function')
+})
---
+++ web-ui/src/util/Authentication.js
@@ -0,0 +1,52 @@
+const baseUrl = "http://localhost:3000";
+
+class ActionResult {
+  constructor(success = true, data) {
+    this.success = success;
+    this.data = data;
+  }
+}
+
+export const login = async (username, password) => {
+  try {
+    const response = await fetch(`${baseUrl}/login`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await response.json();
+
+    if (response.status != 201) {
+      return new ActionResult(false, data);
+    } else {
+      return new ActionResult(true, data);
+    }
+  } catch (err) {
+    return new ActionResult(false, err);
+  }
+};
+
+
+
+export const register = async (name, username, email, phone, password) => {
+  try {
+    const response = await fetch(`${baseUrl}/users`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ name, username, email, phone, password }),
+    });
+    const data = await response.json();
+
+    if (response.status != 201) {
+      return new ActionResult(false, data);
+    } else {
+      return new ActionResult(true, data);
+    }
+  } catch (err) {
+    return new ActionResult(false, err);
+  }
+};
