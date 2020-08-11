6a75c11808fdfab821f78dd4807728204118dece
jason-nordheim <jason.nordheim@gmail.com>
2020-08-11 12:02:43 -0600

    wrote with auth hook

--- web-ui/src/hooks/useAuthentication.js
+++ web-ui/src/hooks/useAuthentication.js
@@ -1,57 +1,72 @@
-import { useReducer } from 'react';
-import Axios from 'axios';
-
-const baseUrl = 'http://localhost:3000';
+import { useReducer } from "react";
+import { useEffect } from 'react'
+import { register, login } from '../util/Authentication'
 
 export const defaultState = {
-	token : null,
-	error : null
-};
-
-export const ACTIONS = {
-	Logout   : 'logout',
-	Login    : 'login',
-	Register : 'Register'
+  isLoading: false,
+  isLoggedIn: false,
+  token: null,
+  error: null,
 };
 
 const authenticationReducer = async (state, action) => {
-	switch (action.type) {	
-		case ACTIONS.login:
+  switch (action.type) {
+		case 'reset': 
+			return action.payload
+    case 'login':
+
+      return {
+        ...state,
+        error: null,
+        isLoading: true,
+      };
+    case 'success':
+      return {
+        ...state,
+        isLoading: false,
+				isLoggedIn: true,
+				token: action.payload
+			};
+		case 'error': 
 			return {
 				...state, 
+				isLoading: false, 
+				isLoggedIn: false, 
+				error: action.payload
 			}
-			break;
-		case ACTIONS.logout:
-			return {
-				...state,
-				token : null,
-				error : null
-			};
-		case ACTIONS.register:
-			await Axios.post('/users', action.payload)
-				.then((res) => {
-					console.log(res);
-					return {
-						...state
-					};
-				})
-				.catch((err) => {
-					return {
-						...state,
-						error : err.message
-					};
-				});
-			break;
-		default:
-			return {
-				...state
-			};
-	}
+    case 'logout':
+      return {
+        ...state,
+        token: null,
+        error: null,
+        isLoading: false,
+        isLoggedIn: false,
+      };
+    case 'register':
+      return {
+        ...state,
+      };
+    default:
+      return state 
+  }
 };
 
 const useAuthentication = (initialState = defaultState) => {
-	const [ state, dispatch ] = useReducer(authenticationReducer, initialState);
-	return [ state, dispatch ];
+	const [state, dispatch] = useReducer(authenticationReducer, initialState);
+
+	// pull state from local storage on load 
+	useEffect(() => {
+		const rawData = localStorage.getItem('data')
+		dispatch({ type: 'reset', payload: JSON.parse(rawData)})
+	}, [])
+
+	// save state to local storage, whenever it changes 
+	useEffect(() => {
+		localStorage.setItem('data', JSON.stringify(state))
+	}, [state])
+	
+
+  return [state, dispatch];
 };
 
 export default useAuthentication;
