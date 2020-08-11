ab7085019e5391816ac15a5804b99b249939a2e2
jason-nordheim <jason.nordheim@gmail.com>
2020-08-11 15:43:27 -0600

    created loggedInHomePage component

--- web-ui/src/Components/LoggedInHomePage.js
+++ web-ui/src/Components/LoggedInHomePage.js
@@ -1,10 +1,27 @@
 import React, { useContext } from 'react' 
+import { Grid, Typography } from '@material-ui/core'
 import { AuthorizationContext } from '../App'
 
 
 const LoggedInHomePage = () => {
   const AuthContext = useContext(AuthorizationContext)
 	return (
-    
-    )
+    <Grid container>
+      <Grid item container xs={12} md={6} justify="center">
+        <Typography variant="h5">Locations</Typography>
+      </Grid>
+      <Grid item container xs={12} md={6} justify="center">
+        <Typography variant="h5">Vendors</Typography>
+      </Grid>
+      <Grid item container xs={12} md={6} justify="center">
+        <Typography variant="h5">Items</Typography>
+      </Grid>
+      <Grid item container xs={12} md={6} justify="center">
+        <Typography variant="h5">Summary</Typography>
+      </Grid>
+    </Grid>
+  );
 };
+
+
+export default LoggedInHomePage
