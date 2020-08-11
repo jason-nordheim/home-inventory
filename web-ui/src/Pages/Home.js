83930ae772e299143d19141cc7a92b7e33a2d116
jason-nordheim <jason.nordheim@gmail.com>
2020-08-11 15:42:59 -0600

    changed home page to display differrent components depending on log in status

--- web-ui/src/Pages/Home.js
+++ web-ui/src/Pages/Home.js
@@ -2,19 +2,14 @@ import React, { useContext} from 'react';
 import Layout from '../Components/Layout';
 import About from '../Components/About';
 import { AuthorizationContext } from '../App';
-import { Typography } from '@material-ui/core';
-
+import LoggedInHomePage from '../Components/LoggedInHomePage'
 
 const HomePage = () => {
 	const authContext = useContext(AuthorizationContext)
 	return (
 		<Layout title="Home">
 					{authContext.state.token === null &&  <About /> } 
-					{authContext.state.token !== null &&(
-						<Typography paragraph>
-							Placeholder for end-user information
-						</Typography>
-						) }
+					{authContext.state.token !== null &&  <LoggedInHomePage />  }
 		</Layout>
 	);
 };
