25cfec947124d6b97c51cdeafa76b1e6423915c2
jason-nordheim <jason.nordheim@gmail.com>
2020-08-11 14:54:11 -0600

    created logged in homepage

---
+++ web-ui/src/Components/LoggedInHomePage.js
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react' 
+import { AuthorizationContext } from '../App'
+
+
+const LoggedInHomePage = () => {
+  const AuthContext = useContext(AuthorizationContext)
+	return (
+    
+    )
+};
