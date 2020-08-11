4967a5e31e4d41264399fbf2050a8328fc7f5eff
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-11 09:32:56 -0600

    created and tested login function

---
+++ web-ui/src/__tests__/Login.Test.js
@@ -0,0 +1,34 @@
+import { login } from '../util/Login'
+
+const username = 'jnordheim'
+const password = 'password' 
+
+test('Login is a function', () => {
+    expect(typeof(login)).toBe('function')
+})
+
+test('login does not throw', () => {
+    return login(username, password)
+        .then(actionResult => {
+            expect(actionResult.data).not.toBeNull()
+            expect(actionResult.success).toBe(true)
+
+        })
+})
+
+test('login returns token', () => {
+    return login(username, password)
+        .then(actionResult => {
+            expect(actionResult.success).toBe(true)
+            expect(actionResult.data).not.toBeNull()
+            expect(actionResult.data.token).not.toBeNull()
+        })
+})
+
+test('login fails with invalid credentials', () => {
+    return login('jonny', 'password')
+        .then(actionResult => {
+            expect(actionResult.success).toBe(false)
+            expect(actionResult.data.error).not.toBeNull()
+        })
+})
