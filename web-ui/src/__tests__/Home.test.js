0c379d22af67f426c2c979f1fcc76a7c35daddb2
jason-nordheim <jason.nordheim@gmail.com>
2020-08-11 12:01:18 -0600

    moved tests to a single file (Authentication.test.js)

--- web-ui/src/__tests__/Home.test.js
+++
@@ -1,3 +0,0 @@
-test('Home test file is functional', () => {
-    expect(1).toEqual(1)
-})
--- web-ui/src/__tests__/Register.test.js
+++
@@ -1,5 +0,0 @@
-import { register } from "../util/Authentication";
-
-describe('Register is a function', () => {
-  expect(typeof(register)).toBe('function')
-})
