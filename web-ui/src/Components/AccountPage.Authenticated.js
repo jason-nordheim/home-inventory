1823fab5d7021f09be822fe9458bad957d78fa99
jason-nordheim <jason.nordheim@gmail.com>
2020-08-12 11:34:45 -0600

    changed authenticated Accont page (more work todo)

--- web-ui/src/Components/AccountPage.Authenticated.js
+++ web-ui/src/Components/AccountPage.Authenticated.js
@@ -1,13 +1,20 @@
-import React, { useState, useContext } from "react";
-import { Grid } from "@material-ui/core";
-import { AuthorizationContext } from "../App";
+import React from "react";
+import { Grid, Typography, Paper, TextField } from "@material-ui/core";
+import useFormStyles from '../style/useFormStyles'
+import { useState } from 'react'
 
-const AuthenticatedAccountPage = () => {
-  const AuthContext = useContext(AuthorizationContext);
 
+const AuthenticatedAccountPage = ({ user }) => {
+  const classes = useFormStyles()
   return (
     <Grid container justify="center">
-      <Grid item xs={11} md={5}>Welcome</Grid>
+      <Grid item xs={11} md={8}>
+        <Paper>
+            <Grid item>
+              <Typography variant="h4">Welcome {user.name}</Typography>
+            </Grid>
+        </Paper>
+      </Grid>
     </Grid>
   );
 };
