a4b629114b18f82eb2534639e235f26f9ec44a5a
jason-nordheim <jason.nordheim@gmail.com>
2020-08-12 11:35:03 -0600

    removed conditional null render from unauthenticated account page

--- web-ui/src/Components/AccountPage.UnAuthenticated.js
+++ web-ui/src/Components/AccountPage.UnAuthenticated.js
@@ -6,15 +6,6 @@ import { AuthorizationContext } from "../App";
 
 const UnAuthenticatedAccountPage = () => {
   const [register, setRegister] = useState(false);
-  const AuthContext = useContext(AuthorizationContext);
-
-  /**
-   * return nothing if no one is logged in
-   */
-  if(AuthContext.state.token !== null) {
-    return null 
-  }
-
   /**
    * If not logged in, provide options to sign into an account 
    * or create a new one 
