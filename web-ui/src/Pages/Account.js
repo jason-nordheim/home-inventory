f7f28e96f9f7f04c28d1d97ee4ebb6892659801e
jason-nordheim <jason.nordheim@gmail.com>
2020-08-12 11:34:00 -0600

    added context to account page to conditionally render different components

--- web-ui/src/Pages/Account.js
+++ web-ui/src/Pages/Account.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../Components/Layout";
-import AuthenticatedAccountPage from '../Components/AccountPage.Authenticated'
-import UnAuthenticatedAccountPage from '../Components/AccountPage.UnAuthenticated'
+import AuthenticatedAccountPage from "../Components/AccountPage.Authenticated";
+import UnAuthenticatedAccountPage from "../Components/AccountPage.UnAuthenticated";
+import { AuthorizationContext } from "../App";
 
-const AccountPage = () => 
-  <Layout title="Account">
-    <UnAuthenticatedAccountPage /> 
-    <AuthenticatedAccountPage /> 
-  </Layout>
+const AccountPage = () => {
+  const AuthContext = useContext(AuthorizationContext);
+  return (
+    <Layout title="Account">
+      { AuthContext.state.user === null ? (
+        <AuthenticatedAccountPage user={AuthContext.state.user} />
+      ) : (
+        <UnAuthenticatedAccountPage />
+      )}
+    </Layout>
+  );
+};
 
 export default AccountPage;
