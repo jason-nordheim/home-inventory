261825c5c288d3d4a38c4c3f1a4612d6970d6488
jason-nordheim <jason.nordheim@gmail.com>
2020-08-13 15:26:41 -0600

    removed unused elements/imports

--- web-ui/src/Components/Home/Home.Authenticated.js
+++ web-ui/src/Components/Home/Home.Authenticated.js
@@ -10,35 +10,12 @@ import {
 } from "@material-ui/core";
 import { AuthorizationContext } from "../../App";
 import QuadrantSummary from './Quadrant/QuadrantSummary'
-import { useState } from 'react'
 import LocationQuadrant from './Quadrant/LocationQuadrant'
 import VendorsQuadrant from './Quadrant/VendorsQuadrant'
 import ItemsQuadrant from './Quadrant/ItemsQuadrant'
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {},
-//   quad: {
-//     width: "100%",
-//     minHeight: "100px",
-//     padding: "1rem",
-//     margin: "1rem",
-//     flex: 1, 
-//   },
-//   quadItemArea: {
-//     height: '120px', 
-//     overflow: 'hidden'
-//   }, 
-//   list: {
-//     width: '100%'
-//   }, 
-//   listItem: {
-//     width: '100%'
-//   }
-// }));
-
 const HomeAuthenticated = () => {
   const AuthContext = useContext(AuthorizationContext);
-  const [locationFrontIsVisible, setLocationFrontIsVisible] = useState(true);
 
   return (
     <div className="homeAuthenticated__container">
