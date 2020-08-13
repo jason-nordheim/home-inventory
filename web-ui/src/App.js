b007adaeaca0916566d3356fd8fe0de257077af1
jason-nordheim <jason.nordheim@gmail.com>
2020-08-12 18:27:01 -0600

    moved to an animated switch

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SitePages from './data/SitePages';
 import useAuthentication, { defaultState } from './hooks/useAuthentication'
+import { AnimatedSwitch } from "react-router-transition";
 
 
 export const AuthorizationContext = React.createContext(defaultState)
@@ -9,20 +10,28 @@ export const AuthorizationContext = React.createContext(defaultState)
 const App = () => {
 	const AuthContext = useAuthentication(defaultState)
 	return (
-		<AuthorizationContext.Provider value={AuthContext} >
-			<BrowserRouter>
-				<Switch>
-						{SitePages.map((link) => {
-							return (
-								<Route key={link.url} path={link.url} exact={link.exact}>
-									{link.comp}
-								</Route>
-							);
-						})}
-				</Switch>
-			</BrowserRouter>
-		</AuthorizationContext.Provider>
-	);
+    <AuthorizationContext.Provider value={AuthContext}>
+      <BrowserRouter>
+        <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1, transition: 'linear', transitionDuration: '0.2s'}}
+        >
+          {SitePages.map((link) => {
+            return (
+              <Route
+                key={link.url}
+                path={link.url}
+                exact={link.exact}
+              >
+                {link.comp}
+              </Route>
+            );
+          })}
+        </AnimatedSwitch>
+      </BrowserRouter>
+    </AuthorizationContext.Provider>
+  );
 };
 
 export default App;
