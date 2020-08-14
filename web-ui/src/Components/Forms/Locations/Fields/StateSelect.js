f3f6f242d2ae50eebe7bea264f3370dae824b227
jason-nordheim <jason.nordheim@gmail.com>
2020-08-14 09:40:42 -0600

    created state select for new locations

---
+++ web-ui/src/Components/Forms/Locations/Fields/StateSelect.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import UsStates from "../../../../data/UsStates";
+
+const StateSelect = ({
+  state,
+  stateError,
+  onChange,
+  required = true,
+  disabled = false,
+  variant = "outlined",
+  label = "State",
+  id = "state",
+}) => {
+  return (
+    <FormControl size="small" fullWidth>
+      <Select
+        label={label}
+        variant={variant}
+        fullWidth
+        id={id}
+        required={required}
+        value={state}
+        onChange={onChange}
+        disabled={disabled}
+      >
+        {UsStates.map((state) => (
+          <MenuItem key={state.abbreviation} value={state.abbreviation}>
+            {" "}
+            {state.name}{" "}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default StateSelect;
