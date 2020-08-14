46774798a9b96ff301ac5c0b8871fc58e82e6a0b
jason-nordheim <jason.nordheim@gmail.com>
2020-08-14 10:55:27 -0600

    added submit button to the location form

--- web-ui/src/Components/Forms/Locations/LocationForm.js
+++ web-ui/src/Components/Forms/Locations/LocationForm.js
@@ -12,9 +12,11 @@ import {
 } from "../../../util/FormValidations";
 import ZipCodeTextField from "./Fields/ZipCodeTextField";
 import LocationTypeSelect from "./Fields/LocationTypeSelect";
+import { Button } from '@material-ui/core'
 
-const LocationForm = () => {
-  const blank = "";
+const blank = "";
+
+const LocationForm = ({ onSubmit, submitText="Submit" }) => {
   const [name, setName] = useState(blank);
   const [nameError, setNameError] = useState(null);
   const [street1, setStreet1] = useState(blank);
@@ -28,45 +30,67 @@ const LocationForm = () => {
   const [type, setType] = useState(null);
   const [typeError, setTypeError] = useState(null);
 
+  const onFormSubmit = e => {
+    e.preventDefault() 
+    onSubmit(e) 
+  }
+
   return (
     <div className="newLocationForm__container">
       <form>
-        <LocationNameField
-          label="Name of Location"
-          locationName={name}
-          error={nameError}
-          onChange={(e) => locationNameChanged(e, setName, setNameError)}
-        />
-        <StreetTextField
-          label="Street 1"
-          street={street1}
-          error={street1Error}
-          onChange={(e) => streetOneChanged(e, setStreet1, setStreet1Error)}
-        />
-        <StreetTextField
-          label="Street 2"
-          street={street2}
-          error={street2Error}
-          required={false}
-          onChange={(e) => streetTwoChanged(e, setStreet2, setStreet2Error)}
-        />
-        <StateSelect
-          label="State"
-          state={state}
-          error={stateError}
-          onChange={(e) => setState(e.target.value)}
-        />
-        <ZipCodeTextField
-          label="Zip Code"
-          value={zip}
-          error={zipError}
-          onChange={(e) => zipCodeChanged(e, setZip, setZipError)}
-        />
-        <LocationTypeSelect
-          value={type}
-          erorr={typeError}
-          onChange={(e) => locationTypeChanged(e, setType, setTypeError)}
-        />
+        <div className="newLocationForm__textContainer">
+          <LocationNameField
+            label="Name of Location"
+            locationName={name}
+            error={nameError}
+            onChange={(e) => locationNameChanged(e, setName, setNameError)}
+          />
+        </div>
+        <div className="newLocationForm__textContainer">
+          <StreetTextField
+            label="Street 1"
+            street={street1}
+            error={street1Error}
+            onChange={(e) => streetOneChanged(e, setStreet1, setStreet1Error)}
+          />
+        </div>
+        <div className="newLocationForm__textContainer">
+          <StreetTextField
+            label="Street 2"
+            street={street2}
+            error={street2Error}
+            required={false}
+            onChange={(e) => streetTwoChanged(e, setStreet2, setStreet2Error)}
+          />
+        </div>
+        <div className="newLocationForm__stateZipContainer">
+          <div className="newLocationForm__stateContainer">
+            <StateSelect
+              label="State"
+              state={state}
+              error={stateError}
+              onChange={(e) => setState(e.target.value)}
+            />
+          </div>
+          <div className="newLocationForm__zipContainer">
+            <ZipCodeTextField
+              label="Zip Code"
+              value={zip}
+              error={zipError}
+              onChange={(e) => zipCodeChanged(e, setZip, setZipError)}
+            />
+          </div>
+        </div>
+        <div className="newLocationForm__typeContainer">
+          <LocationTypeSelect
+            value={type}
+            erorr={typeError}
+            onChange={(e) => locationTypeChanged(e, setType, setTypeError)}
+          />
+        </div>
+        <div className="newLocationForm__submitContainer">
+          <Button variant="contained" onClick={onFormSubmit}>{submitText} </Button>
+        </div>
       </form>
     </div>
   );
