f1d156320d820ff8bad2a2437fbed9b2b02aacd7
jason-nordheim <jason.nordheim@gmail.com>
2020-08-14 13:28:44 -0600

    created new method to post to locations and create a new locaiton for the user with the token provided to the function

--- web-ui/src/util/Authentication.js
+++ web-ui/src/util/Authentication.js
@@ -80,3 +80,42 @@ export const getUserInfo = async (token) => {
   const data = await response.json();
   return data;
 };
+
+/**
+ * Attempts to create a new location for the user associated with the 
+ * provided bearer token 
+ * @param {string} token 
+ * @param {string} name 
+ * @param {string} street1 
+ * @param {string} street2 
+ * @param {string} city 
+ * @param {string} state 
+ * @param {string} zip 
+ * @returns {ActionResult}  
+ */
+export const postNewLocation = async (token, name, street1, street2, city, state, zip ) => {
+  try {
+    const response = await fetch(`${baseUrl}/locations`, {
+      method: 'POST', 
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      }, 
+      body: JSON.stringify(data) 
+    })
+    const data = await response.json() 
+
+    if (response.status !== 201) {
+      return new ActionResult(false, data);
+    } else {
+      return new ActionResult(true, data);
+    }
+  } catch (err) {
+    return new ActionResult(false, err);
+  }
+}
+
+export const updateLocation = async () => {
+  throw new Error('Not Implemented')
+}
+
