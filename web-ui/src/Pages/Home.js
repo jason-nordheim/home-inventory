66315a5ef8ebcd72ae0add9911b1acfcb4d62781
jason-nordheim <jason.nordheim@gmail.com>
2020-08-15 10:27:49 -0600

    changed null comparison to === from ==

--- web-ui/src/Pages/Home.js
+++ web-ui/src/Pages/Home.js
@@ -5,11 +5,11 @@ import HomeAuthenticated from '../Components/Home/Home.Authenticated'
 import HomeUnAuthenticated from '../Components/Home/Home.UnAuthenticated'
 
 
-const HomePage = () => {
+const HomePage = () => {  
 	const authContext = useContext(AuthorizationContext)
 	return (
     <Layout title="Home">
-      {authContext.state.token === null && <HomeUnAuthenticated />}
+      {authContext.state.token == null && <HomeUnAuthenticated />}
       {authContext.state.token !== null && <HomeAuthenticated />}
     </Layout>
   );
