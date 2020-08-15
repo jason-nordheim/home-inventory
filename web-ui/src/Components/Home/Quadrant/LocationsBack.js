897e3739e8236b7c7046e530101e68e09006d7f1
jason-nordheim <jason.nordheim@gmail.com>
2020-08-15 17:05:34 -0600

    moved location back to its own file

---
+++ web-ui/src/Components/Home/Quadrant/LocationsBack.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LocationForm from "../../Forms/Locations/LocationForm";
+
+export const LocationsBack = () => {
+  return (
+    <LocationForm
+      submitText="create"
+    />
+  );
+};
