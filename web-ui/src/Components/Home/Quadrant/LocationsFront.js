9c0695f9caba8e770a250677e3f3a5fc7242b8ce
jason-nordheim <jason.nordheim@gmail.com>
2020-08-15 17:06:38 -0600

    moved location back to its own file

---
+++ web-ui/src/Components/Home/Quadrant/LocationsFront.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { List } from '@material-ui/core'
+/**
+ * Component to be placed in the main content section of the 
+ * Location Quadrant 
+ */
+export const LocationsFront = () => {
+  return (
+    <List>
+
+    </List>
+  )
+}
+
+
+export default LocationsFront
