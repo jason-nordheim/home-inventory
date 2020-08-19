773d8dfc04fe33f7c8f534613cc490034833995e
jason-nordheim <jason.nordheim@gmail.com>
2020-08-19 13:20:45 -0600

    added phoneTextField to vendor form

--- web-ui/src/Components/Forms/Vendor/VendorForm.js
+++ web-ui/src/Components/Forms/Vendor/VendorForm.js
@@ -2,26 +2,37 @@ import React from 'react'
 import { Typography } from '@material-ui/core'
 import { useState } from 'react'
 import NameTextField from '../Fields/NameTextField'
+import PhoneTextField from '../Fields/PhoneTextField'
 
 
 const VendorFrom = ({title="New Vendor"}) => {
   const [vendorName, setVendorName] = useState('')
+  const [vendorPhone, setVendorPhone] = useState('')
 
   return (
     <div className="vendorForm__container">
       <form>
         <div className="vendorForm__header">
-          <Typography variant="h6">
-            {title} 
-          </Typography>
+          <Typography variant="h6">{title}</Typography>
         </div>
         <div className="vendorForm__textField">
-          <NameTextField name={vendorName} nameError={null} onChange={ e=> setVendorName(e.target.value)} /> 
+          <NameTextField
+            name={vendorName}
+            error={null}
+            onChange={(e) => setVendorName(e.target.value)}
+          />
         </div>
-       
+        <div className="vendorPhone__textField">
+          <PhoneTextField
+            size="small"
+            phone={vendorPhone}
+            phoneError={null}
+            onChange={(e) => setVendorPhone(e.target.value)}
+          />
+        </div>
       </form>
     </div>
-  )
+  );
 }
 
 export default VendorFrom 
