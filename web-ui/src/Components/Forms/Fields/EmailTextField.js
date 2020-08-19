8f91c97cb78f8553d9df6b8fbdfb59bc0e42704c
jason-nordheim <jason.nordheim@gmail.com>
2020-08-19 13:29:23 -0600

    added additional parameters to the 'EmailTextField' component

--- web-ui/src/Components/Forms/Fields/EmailTextField.js
+++ web-ui/src/Components/Forms/Fields/EmailTextField.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { TextField } from "@material-ui/core";
 
-export const EmailTextField = ({ email, onChange, emailError, required=false, disabled=false, variant="outlined" }) => {
+export const EmailTextField = ({id=null, size="medium", label="email",  email, onChange, emailError, required=false, disabled=false, variant="outlined" }) => {
   return (
       <TextField
+        size={size}
         variant={variant}
         fullWidth
         error={emailError !== null}
-        id="email"
-        label="Email"
+        id={id}
+        label={label}
         defaultValue={email}
         helperText={emailError == null ? "" : emailError}
         onChange={onChange}
