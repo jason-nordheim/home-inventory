0da4cf38cc8f648ff7a34fd72888f41413bdf263
jason-nordheim <jason.nordheim@gmail.com>
2020-08-19 13:21:05 -0600

    added parameterized 'id' and 'label'

--- web-ui/src/Components/Forms/Fields/PhoneTextField.js
+++ web-ui/src/Components/Forms/Fields/PhoneTextField.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { TextField } from "@material-ui/core";
 
-const PhoneTextField = ({ phone, onChange, phoneError, required=true, disabled=false, size="medium", variant="outlined"}) => {
+const PhoneTextField = ({ id=null, label="Phone", phone, onChange, phoneError, required=true, disabled=false, size="medium", variant="outlined"}) => {
   return (
     <TextField
       size={size}
       variant={variant}
       fullWidth
       error={phoneError !== null}
-      id="phone"
-      label="phone"
+      id={id}
+      label={label}
       defaultValue={phone}
       helperText={phoneError == null ? phoneError : null}
       onChange={onChange}
