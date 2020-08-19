4c265b93d9475aaa31e181b8fd078e3dd93e71d9
jason-nordheim <jason.nordheim@gmail.com>
2020-08-19 13:29:06 -0600

    added the look up of all vendors and mapping them to supply a checked value and sending to the 'VendorsFront' component to be displayed as a list of items

--- web-ui/src/Components/Home/Quadrant/VendorsQuadrant.js
+++ web-ui/src/Components/Home/Quadrant/VendorsQuadrant.js
@@ -1,28 +1,60 @@
-import React , { useState } from "react";
-import { Typography } from "@material-ui/core";
+import React, { useState, useContext, useEffect } from "react";
+import { List } from "@material-ui/core";
 import Quadrant from "./Quadrant";
-import VendorForm from '../../Forms/Vendor/VendorForm'
+import VendorsFront from './VendorsFront'
+import { AuthorizationContext } from "../../../App";
+import { VendorsBack } from './VendorsBack'
 
 const VendorsQuadrant = () => {
+  const [AuthState, AuthActions] = useContext(AuthorizationContext);
   const [showFront, setShowFront] = useState(true);
+  const [vendors, setVendors] = useState([]);
+
+  useEffect(() => {
+    updatedVendorList();
+  }, [showFront]);
+
+  const updatedVendorList = () => {
+    AuthActions.vendors.getAll().then((data) => {
+      if (data.iid === undefined) {
+        // data is valid
+        const mappedData = data.map((vendor) => {
+          return { ...vendor, checked: shouldBeChecked(vendor) };
+        });
+        setVendors(mappedData);
+      }
+    });
+  };
+  
+  const shouldBeChecked = (vendor) => {
+    if (vendors !== null && vendors !== []) {
+      // should be checked if it already was in previous load 
+      vendors.forEach(v => {
+        if (v.id === vendor.id && v.checked) {
+          return true 
+        }
+      })
+    }
+    return false // default to not checked 
+  }
+
+  const setChecked = (id) => {
+    const mappedVendors = vendors.map(v => {
+      if(v.id === id) return v.checked = !v.checked
+      else return v
+    })
+    setVendors(mappedVendors)
+  };
 
   return (
     <Quadrant
       title="Vendors"
-      front={<VendorsFront />}
+      front={<VendorsFront vendors={vendors} setChecked={setChecked} />}
       back={<VendorsBack />}
       showFront={showFront}
       setShowFront={setShowFront}
     />
   );
-};
-
-const VendorsFront = () => {
-  return <Typography>Vendors Front</Typography>;
-};
-
-const VendorsBack = () => {
-  return <VendorForm /> 
 };
 
 export default VendorsQuadrant;
