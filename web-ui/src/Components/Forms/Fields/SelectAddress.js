ed822372d2ba260697eebea4d98bf8deb6d5496b
jason-nordheim <jason.nordheim@gmail.com>
2020-08-19 08:53:00 -0600

    Created an address select drop down

---
+++ web-ui/src/Components/Forms/Fields/SelectAddress.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+
+const SelectAddress = ({
+  value,
+  valueError,
+  onChange,
+  menuItems = [], 
+  required = true,
+  disabled = false,
+  variant = "outlined",
+  label = "Address",
+  id = "address",
+}) => {
+  console.log('address', menuItems)
+  return (
+    <FormControl required={required} size="small" fullWidth>
+      <InputLabel className="selectAddress__inputLabel" size="small">
+        {label}
+      </InputLabel>
+      <Select
+        label={label}
+        variant={variant}
+        fullWidth
+        id={id}
+        required={required}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      >
+        {menuItems.map((address) => (
+          <MenuItem key={address.id} value={address.id}>
+            {address.name}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default SelectAddress;
