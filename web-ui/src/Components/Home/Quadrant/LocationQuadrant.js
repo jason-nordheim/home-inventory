ce95f3a61b15012c91e46f06fae70c1acc370151
jason-nordheim <jason.nordheim@gmail.com>
2020-08-19 10:23:53 -0600

    created the locations quadrant

--- web-ui/src/Components/Home/Quadrant/LocationQuadrant.js
+++ web-ui/src/Components/Home/Quadrant/LocationQuadrant.js
@@ -1,18 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Quadrant from "./Quadrant";
 import { LocationsFront } from "./LocationsFront";
 import { LocationsBack } from "./LocationsBack";
+import { AuthorizationContext } from '../../../App'
 
 const LocationsQuadrant = () => {
+  const [AuthState, AuthActions] = useContext(AuthorizationContext)
   const [showFront, setShowFront] = useState(true);
+  const [locations, setLocations] = useState([])
+
+  useEffect(() => {
+    updateLocationList()
+  },[])
+  
+  const setChecked = (location) => {
+    if(locations !== null && locations !== []) {
+      locations.forEach(l => {
+        if (l.id === location.id && l.checked) {
+          return true
+        }
+      })
+    }
+    return false 
+  }
+
+  const updateLocationList = () => {
+    AuthActions.addresses.getAll().then(data => {
+      AuthActions.locations.getAll().then(locations => {
+        const mappedLocations = locations.map((loc) => {
+          const addr = data.addresses.find(addr => addr.id === loc.address_id)
+          return {...loc, checked: setChecked(loc), address: addr }
+        }) 
+        setLocations(mappedLocations)
+      });
+    })
+  }
+
+  const createNewLocation = (locationName, locationType, addressId ) => {
+    AuthActions.locations.create(locationName, locationType, addressId)
+      .then(() => updateLocationList())
+  }
+
+  const onChecked = (id) => {
+    const updatedLocations = locations.map(l => {
+      if (l.id === id) {
+        l.checked = !l.checked
+      }
+      return l 
+    })
+    setLocations(updatedLocations)
+  }
 
   return (
     <Quadrant
       showFront={showFront}
       setShowFront={setShowFront}
       title="Locations"
-      front={<LocationsFront />}
-      back={<LocationsBack/> }
+      front={<LocationsFront locations={locations} setChecked={onChecked} />}
+      back={<LocationsBack createNew={createNewLocation}/> }
     />
   );
 };
