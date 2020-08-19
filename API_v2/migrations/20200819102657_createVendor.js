458cccfbd90e1e93ca85cfdc453c844ad028f900
jason-nordheim <jason.nordheim@gmail.com>
2020-08-19 10:36:17 -0600

    corrected vendor knex migration

--- API_v2/migrations/20200819102657_createVendor.js
+++ API_v2/migrations/20200819102657_createVendor.js
@@ -1,18 +1,13 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("location", (t) => {
+  return knex.schema.createTable("vendor", (t) => {
     t.increments("id");
     t.string("name");
     t.string("type");
     t.integer("user_id").notNullable().references("id").inTable("user").index();
-    t.integer("address_id")
-      .notNullable()
-      .references("id")
-      .inTable("address")
-      .index();
     t.timestamps(true, true);
   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("location");
+  return knex.schema.dropTableIfExists("vendor");
 };
