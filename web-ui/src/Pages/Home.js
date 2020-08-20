fc4af42d6c1cf746ce07e6ccec5e095b0625782c
jason-nordheim <jason.nordheim@gmail.com>
2020-08-20 12:36:12 -0600

    fixed displaying of home page component and simplified code for readability

--- web-ui/src/Pages/Home.js
+++ web-ui/src/Pages/Home.js
@@ -1,16 +1,21 @@
-import React, { useContext} from 'react';
+import React, { useContext, useState, useTimeout } from 'react';
 import Layout from '../Components/Layout';
 import { AuthorizationContext } from '../App';
 import HomeAuthenticated from '../Components/Home/Home.Authenticated'
 import HomeUnAuthenticated from '../Components/Home/Home.UnAuthenticated'
 
 
+/**
+ * Component defintion 
+ */
 const HomePage = () => {  
-	const [AuthState, AuthActions] = useContext(AuthorizationContext)
+  const AuthState = useContext(AuthorizationContext)[0]
+  const authenticated = AuthState.token ? true : false 
+  const title = `${authenticated ? "Dashboard" : "Home"}`; 
+  
 	return (
-    <Layout title="Home">
-      {AuthState.token == null && <HomeUnAuthenticated />}
-      {AuthState.token !== null && <HomeAuthenticated />}
+    <Layout title={title}>
+      { authenticated ? <HomeAuthenticated /> : <HomeUnAuthenticated />}
     </Layout>
   );
 };
