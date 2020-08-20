da750f9470707d716f7a532462500e1c80df6873
jason-nordheim <jason.nordheim@gmail.com>
2020-08-20 12:07:30 -0600

    moved user retrieval to dedicated function

--- web-ui/src/Components/Account/AccountPage.Authenticated.js
+++ web-ui/src/Components/Account/AccountPage.Authenticated.js
@@ -12,22 +12,37 @@ const AuthenticatedAccountPage = () => {
   const [AuthState, AuthActions] = useContext(AuthorizationContext)
   const [user, setUser] = useState({})
 
+  /**
+   * Should retrieve the current user at render 
+   */
   useEffect(() => {
-    if(AuthState.token) {
-      AuthActions.users.MyInfo()
-        .then(data => setUser(data))
-    }
+    getLoggedInUser()
   }, [])
+
+  /**
+   * Retrieves the currently displayed user to 
+   * render a welcome message 
+   */
+  function getLoggedInUser() {
+    if (AuthState.token) {
+      AuthActions.users.MyInfo().then((data) => {
+        setUser(data)
+      });
+    }
+  }
 
   return (
     <div className="authenticatedAccountPage_container">
-        <Paper className="authenticatedAccountPage__paper">
-          <div>
-            <Typography variant="h4">Welcome {user.name}</Typography>
-          </div>
-          <hr />
-          <AccountDetailsAccordian user={user} /> 
-        </Paper>
+      <Paper className="authenticatedAccountPage__paper">
+        <div>
+          <Typography variant="h4">Welcome {user.name}</Typography>
+        </div>
+        <hr />
+        <AccountDetailsAccordian
+          AuthActions={AuthActions}
+          AuthState={AuthState}
+        />
+      </Paper>
     </div>
   );
 };
