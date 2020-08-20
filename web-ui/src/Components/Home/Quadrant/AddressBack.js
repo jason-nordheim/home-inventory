a34e8b9734a6babbee35411137f01aeca34d7085
jason-nordheim <jason.nordheim@gmail.com>
2020-08-20 09:29:57 -0600

    passed additional props to AddressForm component to support edit/patch/update functionality

--- web-ui/src/Components/Home/Quadrant/AddressBack.js
+++ web-ui/src/Components/Home/Quadrant/AddressBack.js
@@ -1,8 +1,16 @@
 import React from "react";
 import AddressForm from "../../Forms/Address/AddressForm";
 
-export const AddressBack = ({ onCreate }) => {
-  return <AddressForm onSuccess={onCreate}  />;
+export const AddressBack = ({address, updateAddress, submitText, createAddress, onSubmit }) => {
+  return (
+    <AddressForm
+      submitText={submitText}
+      createAddress={createAddress}
+      updateAddress={updateAddress}
+      onSubmit={onSubmit}
+      address={address}
+    />
+  );
 };
 
-export default AddressBack
+export default AddressBack;
