0899c9414b6d6819ea53d12b3aa9b68a9fb99035
jason-nordheim <jason.nordheim@gmail.com>
2020-08-19 23:06:06 -0600

    scaffolded out delete function for AddressQuadrant

--- web-ui/src/Components/Home/Quadrant/AddressFront.js
+++ web-ui/src/Components/Home/Quadrant/AddressFront.js
@@ -1,37 +1,12 @@
 import React, { useState, useContext, useEffect } from "react";
 import { List } from "@material-ui/core";
-import { AuthorizationContext } from "../../../App";
 import AddressListItem from "../../../Components/AddressListItem";
 /**
  * Component to be placed in the main content section of the
  * Location Quadrant
  */
-export const AddressFront = ({ oaddressAdded }) => {
-  const [_, AuthActions] = useContext(AuthorizationContext);
-  const [addresses, setAddresses] = useState([]);
-
-  const handleChecked = id => {
-    const updatedAddress = addresses.map(a => {
-      if(a.id === id) a.checked = !a.checked 
-      return a 
-    })
-    setAddresses(updatedAddress)
-  }
-
-  /**
-   * Setup all the address objects to be mapped onto 
-   * AddressListItem components
-   */
-  useEffect(() => {
-    AuthActions.addresses.getAll().then((data) => {
-      setAddresses(
-        data.addresses.map((a) => {
-          return { ...a, checked: false };
-        })
-      );
-    });
-  }, [AuthActions.addresses]);
-
+export const AddressFront = ({ addresses, handleChecked }) => {
+ 
   return (
     <List>
       {addresses !== [] &&
--- web-ui/src/Components/Home/Quadrant/AddressQuadrant.js
+++ web-ui/src/Components/Home/Quadrant/AddressQuadrant.js
@@ -1,18 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Quadrant from "./Quadrant";
 import AddressFront from "./AddressFront";
 import AddressBack from "./AddressBack";
+import { AuthorizationContext } from "../../../App";
 
 export const AddressQuadrant = () => {
   const [showFront, setShowFront] = useState(true);
+  const [_, AuthActions] = useContext(AuthorizationContext);
+  const [addresses, setAddresses] = useState([]);
+
+  const handleChecked = (id) => {
+    const updatedAddress = addresses.map((a) => {
+      if (a.id === id) a.checked = !a.checked;
+      return a;
+    });
+    setAddresses(updatedAddress);
+  };
+
+  /**
+    * Setup all the address objects to be mapped onto
+    * AddressListItem components
+    */
+  useEffect(() => {
+    AuthActions.addresses.getAll().then((data) => {
+      setAddresses(
+        data.addresses.map((a) => {
+          return { ...a, checked: false };
+        })
+      );
+    });
+  }, [AuthActions.addresses]);
+
+
+  const deleteSelected = () => {
+
+  }
 
   return (
     <Quadrant
       showFront={showFront}
       setShowFront={setShowFront}
+      deleteSelected={deleteSelected}
+      deleteDisabled={
+        addresses.reduce((acc, val) => (val.checked ? acc + 1 : acc), 0) == 0
+      }
       title="Addresses"
-      front={<AddressFront />}
-      back={<AddressBack onCreate={() => setShowFront(!showFront) } />}
+      front={
+        <AddressFront addresses={addresses} handleChecked={handleChecked} />
+      }
+      back={<AddressBack onCreate={() => setShowFront(!showFront)} />}
     />
   );
 };
