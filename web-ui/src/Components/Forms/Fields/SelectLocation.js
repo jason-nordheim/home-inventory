960db32156b5e55809b05b92c2f4e7cef56cefd8
jason-nordheim <jason.nordheim@gmail.com>
2020-08-19 20:48:13 -0600

    created select location component

---
+++ web-ui/src/Components/Forms/Fields/SelectLocation.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import { AuthorizationContext } from '../../../App'
+
+const SelectLocation = ({
+  value="",
+  valueError,
+  onChange,
+  menuItems = [], 
+  required = true,
+  disabled = false,
+  variant = "outlined",
+  label = "Location",
+  id = null,
+}) => {
+
+  console.log('menu', menuItems)
+  return (
+    <FormControl required={required} size="small" fullWidth>
+      <InputLabel className="selectAddress__inputLabel" size="small">
+        {label}
+      </InputLabel>
+      <Select
+        label={label}
+        variant={variant}
+        fullWidth
+        id={id}
+        required={required}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      >
+        {menuItems.map((location) => (
+          <MenuItem key={location.id} value={location.id}>
+            {location.name}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default SelectLocation;
