b0d0a2ddc9f75d006ab673915b146f993072a2f2
jason-nordheim <jason.nordheim@gmail.com>
2020-08-20 09:29:32 -0600

    scaffolded out functionality for update/edit/patch functionality

--- web-ui/src/Components/Forms/Address/AddressForm.js
+++ web-ui/src/Components/Forms/Address/AddressForm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import { AuthorizationContext } from "../../../App";
 import NameTextField from "../Fields/NameTextField";
 import StreetTextField from "../Fields/StreetTextField";
@@ -6,79 +6,73 @@ import CityTextField from "../Fields/CityTextField";
 import StateSelect from "../Fields/StateSelect";
 import ZipCodeTextField from "../Fields/ZipCodeTextField";
 import UsStates from "../../../data/UsStates";
-import { Button } from '@material-ui/core'
+import { Button } from "@material-ui/core";
 
-export const AddressForm = ({ variant="CREATE", onSuccess}) => {
-  const [AuthState, AuthActions] = useContext(AuthorizationContext);
-  const blank = "";
-  const [name, setName] = useState(blank);
-  const [street1, setStreet1] = useState(blank);
-  const [street2, setStreet2] = useState(blank);
-  const [city, setCity] = useState("");
-  const [state, setState] = useState(UsStates[0].abbreviation);
-  const [zip, setZip] = useState(blank);
-  const [submitText, setSubmitText] = useState('Submit')
+export const AddressForm = ({
+  address = null,
+  submitText,
+  onSubmit,
+  createAddress,
+  updateAddress,
+}) => {
+  /**
+   * Setup default values
+   *
+   * In the event that this for is being used to edit an address, the form
+   * will be prefilled with the existing information
+   */
+  const defaultValues = {
+    name:
+      address.name !== null || address.name !== undefined ? address.name : "",
+    street1:
+      address.street1 !== null || address.street1 !== undefined
+        ? address.street1
+        : "",
+    street2:
+      address.street2 !== null || address.street2 !== undefined
+        ? address.street2
+        : "",
+    city:
+      address.city !== null || address.city !== undefined ? address.city : "",
+    state:
+      address.state !== null || address.state !== undefined
+        ? address.state
+        : UsStates[0].abbreviation,
+    zip: address.zip !== null || address.zip !== undefined ? address.zip : "",
+  };
 
   /**
-   * Change the text of the submit button depending on the 
-   * variant used. Defaults to creating a new address 
+   * Context
    */
-  useEffect(() => {
-   switch (variant) {
-     case "CREATE":
-       setSubmitText("Submit");
-       break; 
-     case "EDIT":
-       setSubmitText("Save")
-       break; 
-     case "DELETE":
-      setSubmitText("Delete")
-      break; 
-     default:
-       setSubmitText("Submit");
-       break; 
-   }
-  }, [])
+  const [AuthState, AuthActions] = useContext(AuthorizationContext);
 
-  const clearForm = () => {
-    setName('')
-    setStreet1('')
-    setStreet2('')
-    setCity('')
-    setState(UsStates[0].abbreviation)
-    setZip('')
-  }
-
-  
   /*
-   * Handles the submit event (CREATE/EDIT/DELETE) 
+   * State Variables
    */
-  const handleSubmit = e => {
-    e.preventDefault() 
-    switch (variant) {
-      case "CREATE":
-         if (AuthState.token) {
-           AuthActions.addresses
-             .create(name, street1, street2, city, state, zip)
-             .then(() => clearForm());
-           if(typeof(onSuccess) === "function"){
-             onSuccess()
-           }
-           
-         } else {
-           alert('You must be logged in to perform this action')
-         }
-        break; 
-      case "EDIT":
-        break; 
-      case "DELETE":
-        break; 
-      default:
-        break; 
+  const [name, setName] = useState(defaultValues.name);
+  const [street1, setStreet1] = useState(defaultValues.street1);
+  const [street2, setStreet2] = useState(defaultValues.street2);
+  const [city, setCity] = useState(defaultValues.city);
+  const [state, setState] = useState(defaultValues.state);
+  const [zip, setZip] = useState(defaultValues.zip);
+
+  /*
+   * Handles the click event for the address form
+   *
+   * If the address provided via props was null, we should be
+   * creating a new address
+   *
+   * Otherwise we should be editing an existing address
+   */
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (address === null) {
+      createAddress(name, street1, street2, city, state, zip);
+    } else {
+      updateAddress(name, street1, street2, city, state, zip);
     }
+    if (typeof onSubmit === "function") onSubmit();
   }
-
-
 
   return (
     <div className="addressForm__container">
@@ -107,19 +101,30 @@ export const AddressForm = ({ variant="CREATE", onSuccess}) => {
           />
         </div>
         <div className="addressForm__textContainer">
-          <CityTextField error={null} value={city} onChange={e => setCity(e.target.value)} />
+          <CityTextField
+            error={null}
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
         </div>
         <div className="addressForm__textContainer">
           <div className="addressForm__stateContainer">
-            <StateSelect value={state} onChange={e => setState(e.target.value)}/>
+            <StateSelect
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+            />
           </div>
           <div className="addressForm__zipContainer">
-            <ZipCodeTextField value={zip} onChange={e => setZip(e.target.value)} error={null} />
+            <ZipCodeTextField
+              value={zip}
+              onChange={(e) => setZip(e.target.value)}
+              error={null}
+            />
           </div>
         </div>
         <div className="addressForm__buttonContainer">
-          <Button variant="contained" color="primary" onClick={handleSubmit} >
-            { submitText }
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            {submitText}
           </Button>
         </div>
       </form>
