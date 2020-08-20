9d4e8ab395bce25225d00a01d28e009b3902e81d
jason-nordheim <jason.nordheim@gmail.com>
2020-08-20 09:28:48 -0600

    formatting, comments and additional methods to setup the update/patch functionality for addresses

--- web-ui/src/Components/Home/Quadrant/AddressQuadrant.js
+++ web-ui/src/Components/Home/Quadrant/AddressQuadrant.js
@@ -10,32 +10,32 @@ export const AddressQuadrant = () => {
   const [addresses, setAddresses] = useState([]);
 
   /**
-   * Handles the action of "checking" the selected 
+   * Handles the action of "checking" the selected
    * address in the list, by recieving the props
    * of 'id' from the child component
-   * @param {id} id 
+   * @param {id} id
    */
-  function handleChecked(id){
+  function handleChecked(id) {
     const updatedAddress = addresses.map((a) => {
       if (a.id === id) a.checked = !a.checked;
       return a;
     });
     setAddresses(updatedAddress);
-  };
+  }
 
   /**
    * Setup all the address objects to be mapped onto
    * AddressListItem components
    */
   useEffect(() => {
-    updateAddresses() 
+    updateAddresses();
   }, [showFront]);
 
   /**
-   * Makes API call retrieving all the address 
-   * records for the current user 
+   * Makes API call retrieving all the address
+   * records for the current user
    */
-  function updateAddresses(){
+  function updateAddresses() {
     AuthActions.addresses.getAll().then((data) => {
       setAddresses(
         data.addresses.map((a) => {
@@ -46,63 +46,114 @@ export const AddressQuadrant = () => {
   }
 
   /**
-   * Determines if an address should be checked 
-   * @param {string} address 
+   * Determines if an address should be checked by comparing
+   * the checked property of the associated adddress object
+   * from current state with the ID of the address provided
+   * @param {string} address
    */
-  function shouldBeCheced(address){
-     if (addresses !== null && addresses !== []) {
-       addresses.forEach((addr) => {
-         if (addr.id === addr.id && addr.checked) {
-           return true;
-         }
-       });
-     }
-     return false; 
+  function shouldBeCheced(address) {
+    if (addresses !== null && addresses !== []) {
+      addresses.forEach((addr) => {
+        if (addr.id === addr.id && addr.checked) {
+          return true;
+        }
+      });
+    }
+    return false;
   }
 
   /**
-   * Function to handle the request to edit 
-   * the selected address display in the list 
-   * 
-   * updates the list upon completion 
+   * Function to handle the request to edit
+   * the selected address display in the list
+   *
+   * updates the list upon completion
    */
   const editSelected = () => {
-    // TODO 
+    // TODO
+  };
+
+  /**
+   * function to handle the request to delete
+   * one or more addresses from the list
+   *
+   * updates the list upon completion
+   */
+  const deleteSelected = () => {
+    const selected_addresses = addresses.reduce(
+      (acc, val) => (val.checked ? [...acc, val] : acc),
+      []
+    );
+    selected_addresses.forEach((addr) => {
+      AuthActions.addresses.delete(addr.id);
+    });
+    updateAddresses();
+  };
+
+  /**
+   * Makes call to the database creating a new address to be associated
+   * with the currently authenticated user
+   *
+   * updates the address list when complete
+   * @param {string} name
+   * @param {string} street1
+   * @param {string} street2
+   * @param {string} city
+   * @param {string} state
+   * @param {string} zip
+   */
+  function createAddress(name, street1, street2, city, state, zip) {
+    AuthActions.addresses.create(name, street1, street2, city, state, zip);
+    updateAddresses();
+  }
+
+  /**
+   * Makes update/patch request to API to update the values of the
+   * edited addresss 
+   * 
+   * requests updated address list when complete 
+   * @param {string} name 
+   * @param {string} street1 
+   * @param {string} street2 
+   * @param {string} city 
+   * @param {string} state 
+   * @param {string} zip 
+   */
+  function updateAddress(name, street1, street2, city, state, zip){
+    // todo 
   }
 
 
-  /**
-   * function to handle the request to delete 
-   * one or more addresses from the list 
-   * 
-   * updates the list upon completion 
+  /*
+   * Need to frequently reference the number checked to determine
+   * which buttons should be enabled
    */
-  const deleteSelected = () => {
-    const selected_addresses = addresses.reduce((acc, val) => (val.checked ? [...acc, val]: acc), [])
-    selected_addresses.forEach(addr => {
-      AuthActions.addresses.delete(addr.id)
-    })
-    updateAddresses()
-  };
+  const num_checked = addresses.reduce(
+    (acc, val) => (val.checked ? acc + 1 : acc),
+    0
+  );
 
   /*
-   * Need to frequently reference the number checked to determine 
-   * which buttons should be enabled  
+   * Child Components with props
    */
-  const num_checked = addresses.reduce((acc, val) => (val.checked ? acc + 1 : acc), 0)
-
-  /*
-   * Child Components with props  
-   */
-  const front = <AddressFront addresses={addresses} handleChecked={handleChecked} />
-  const back = <AddressBack onCreate={() => setShowFront(!showFront)} />;
+  const front = (
+    <AddressFront addresses={addresses} handleChecked={handleChecked} />
+  );
+  const back = (
+    <AddressBack
+      onSubmit={() => setShowFront(!showFront)}
+      address={null}
+      createAddress={createAddress}
+      updateAddress={updateAddress}
+      submitText={"Submit"}
+    />
+  );
 
   return (
     <Quadrant
       showFront={showFront}
       setShowFront={setShowFront}
       deleteSelected={deleteSelected}
-      deleteDisabled={num_checked === 0 }
+      deleteDisabled={num_checked === 0}
       editDisabled={num_checked !== 1}
       editSelected={editSelected}
       title="Addresses"
