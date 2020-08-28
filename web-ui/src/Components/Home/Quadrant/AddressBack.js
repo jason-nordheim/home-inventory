ff83332c5f2bf265f220a7addc53785927c2e149
jason-nordheim <jason.nordheim@gmail.com>
2020-08-27 21:43:48 -0600

    removed unused imports

--- web-ui/src/Components/Home/Quadrant/AddressBack.js
+++ web-ui/src/Components/Home/Quadrant/AddressBack.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React from "react";
 import AddressForm from "../../Forms/Address/AddressForm";
 
 export const AddressBack = ({address, updateAddress, submitText, createAddress, onSubmit }) => {
