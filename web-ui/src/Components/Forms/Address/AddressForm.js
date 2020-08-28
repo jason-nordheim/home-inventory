d03f46ed9295847652020dfd024aeaa8ac539bd2
jason-nordheim <jason.nordheim@gmail.com>
2020-08-27 21:43:08 -0600

    removed unused imports

--- web-ui/src/Components/Forms/Address/AddressForm.js
+++ web-ui/src/Components/Forms/Address/AddressForm.js
@@ -1,5 +1,4 @@
-import React, { useContext, useState } from "react";
-import { AuthorizationContext } from "../../../App";
+import React, { useState } from "react";
 import NameTextField from "../Fields/NameTextField";
 import StreetTextField from "../Fields/StreetTextField";
 import CityTextField from "../Fields/CityTextField";
